51b4435b07e7286ff467ddeb800711d094895f15
vamshi <burravamshi26@gmail.com>
2024-10-29 14:30:51 +0530

    edited to sample.java

--- sample.java
+++ sample.java
@@ -1,8 +1,7 @@
 public class sample
 {
-	public void static main(String args[])
-	{
-		System.out.println("hello world");
-	}
+public static void main(String args[])
+{
+System.out.println("Gujjula Deepak");
 }
-
+}
